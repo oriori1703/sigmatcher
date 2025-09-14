oriori1703/sigmatcher

commit a5339f369b743dea096220438b082680dfa3ae79

Only search files that previously matched the previous class signatures

src/sigmatcher/analysis.py
@@ -113,10 +113,34 @@ class ClassAnalyzer(Analyzer):
     search_root: Path
 
     def analyze(self, results: Dict[str, Union[Result, Exception]]) -> MatchedClass:
-        signatures = self.get_signatures_for_version()
-        class_matches = filter_signature_matches(
-            (signature, signature.resolve_macros(results).check_files([self.search_root])) for signature in signatures
-        )
+        signatures = list(self.get_signatures_for_version())
+
+        # Make sure the first signature is a whitelist signature in order to improve performance
+        whitelist_signature_index = 0
+        for i, signature in enumerate(signatures):
+            if signature.count != 0:
+                whitelist_signature_index = i
+                break
+        signatures.insert(0, signatures.pop(whitelist_signature_index))
+
+        class_matches = set(self.search_root.rglob("*.smali"))
+
+        for signature in signatures:
+            # Limit the search avoid too many arguments to ripgrep
+            if len(class_matches) < 100:
+                search_paths = list(class_matches)
+            else:
+                search_paths = [self.search_root]
+
+            # TODO: Dedeuplicate this and filter_signature_matches()
+            matches = signature.resolve_macros(results).check_files(search_paths)
+            if signature.count == 0:
+                class_matches.difference_update(matches)
+            else:
+                class_matches.intersection_update(matches)
+                if len(class_matches) == 0:
+                    break
+
         self.check_match_count(class_matches)
         match = next(iter(class_matches))
         with match.open() as f:
@@ -225,14 +249,15 @@ class ExportAnalyzer(Analyzer):
 
 
 def create_analyzers(
-    definitions: Sequence[ClassDefinition], unpacked_path: Path, app_version: Optional[str]
+    definitions: Sequence[ClassDefinition], search_root: Path, app_version: Optional[str]
 ) -> Dict[str, Analyzer]:
+    canonical_search_root = search_root.resolve()
     name_to_analyzer: Dict[str, Analyzer] = {}
     for class_definition in definitions:
         if not class_definition.is_in_version_range(app_version):
             continue
 
-        class_analyzer = ClassAnalyzer(class_definition, app_version, unpacked_path)
+        class_analyzer = ClassAnalyzer(class_definition, app_version, canonical_search_root)
         name_to_analyzer[class_definition.name] = class_analyzer
 
         for method_definition in class_definition.methods:

commit cf38e6d459215961802aa5469dcc2dfcd0b412a1

Fix a performance regression by caching the list of smali files
The regression was caused by a5339f369b743dea096220438b082680dfa3ae79,
because `rglob`ing all of the files each time is expensive.

Thank you @Bip901 for figuaring out the issue and inspiring the fix
by creating https://github.com/oriori1703/sigmatcher/pull/35

Co-Authored-By: Bip901 <74911767+Bip901@users.noreply.github.com>

src/sigmatcher/analysis.py
@@ -2,6 +2,7 @@ import dataclasses
 import graphlib
 from abc import ABC, abstractmethod
 from collections.abc import Callable, Iterable, Sequence
+from functools import cache
 from pathlib import Path
 from typing import TypeVar
 
@@ -47,10 +48,10 @@ SignatureMatch = TypeVar("SignatureMatch", str, Path)
 
 def filter_signature_matches(
     signatures: Iterable[Signature],
-    initial_matches: set[SignatureMatch],
+    initial_matches: Iterable[SignatureMatch],
     check_signature_callback: Callable[[Signature, set[SignatureMatch]], list[SignatureMatch]],
 ) -> set[SignatureMatch]:
-    all_matches: set[SignatureMatch] = initial_matches
+    all_matches: set[SignatureMatch] = set(initial_matches)
     for signature in signatures:
         signature_match = check_signature_callback(signature, all_matches)
         if signature.count == 0:
@@ -61,6 +62,11 @@ def filter_signature_matches(
                 break
 
     return all_matches
+
+
+@cache
+def get_smali_files(search_root: Path) -> frozenset[Path]:
+    return frozenset(search_root.rglob("*.smali"))
 
 
 @dataclasses.dataclass(frozen=True)
@@ -132,7 +138,7 @@ class ClassAnalyzer(Analyzer):
 
             return signature.resolve_macros(results).check_files(search_paths)
 
-        initial_matches = set(self.search_root.rglob("*.smali"))
+        initial_matches = get_smali_files(self.search_root)
         class_matches = filter_signature_matches(signatures, initial_matches, check_signature_callback)
 
         self.check_match_count(class_matches)