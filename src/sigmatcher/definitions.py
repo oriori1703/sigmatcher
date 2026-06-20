bd08b73cc487135d443e64181f912a945529035c
orip <oriori1703@gmail.com>
2026-06-20 19:33:48 +0300

    Remove a pyrefly ignore that is no longer necessary

--- src/sigmatcher/definitions.py
+++ src/sigmatcher/definitions.py
@@ -297,7 +297,7 @@ def merge_definition(def1: TDefinition, def2: TDefinition) -> TDefinition:
 
     if isinstance(def1, ClassDefinition):
         assert isinstance(def2, ClassDefinition)
-        return def1.model_copy(  # pyrefly: ignore[bad-return] related to https://github.com/facebook/pyrefly/issues/1274
+        return def1.model_copy(
             update={
                 "signatures": signatures,
                 "methods": merge_definitions_groups([def1.methods, def2.methods]),
